adbc3ee9d290871363c949807882bbc8f67b239a
deviljin112 <39461646+deviljin112@users.noreply.github.com>
2019-03-19 20:58:20 +0000

    Added missing file
    forgot to add items.py

---
+++ items.py
@@ -0,0 +1,265 @@
+items = {
+
+    "head"   : {
+        
+        1   : {
+            "name"      : "Woolen Hat",
+            "slot"      : "head",
+            "defence"   : 2
+        },
+
+        2   : {
+            "name"      : "Wooden Bowl",
+            "slot"      : "head",
+            "defence"   : 4
+        },
+
+        3   : {
+            "name"      : "Glass Bowl",
+            "slot"      : "head",
+            "defence"   : 1
+        },
+
+        4   : {
+            "name"      : "Steel Helmet",
+            "slot"      : "head",
+            "defence"   : 5
+        },
+
+        5   : {
+            "name"      : "Leather Cap",
+            "slot"      : "head",
+            "defence"   : 3
+        }
+    },
+
+    "torso"    : {
+
+        1   : {
+            "name"      : "Wooden Board",
+            "slot"      : "torso",
+            "defence"   : 1
+        },
+
+        2   : {
+            "name"      : "Chainmail",
+            "slot"      : "torso",
+            "defence"   : 4
+        },
+
+        3   : {
+            "name"      : "Leather Scale",
+            "slot"      : "torso",
+            "defence"   : 2
+        },
+
+        4   : {
+            "name"      : "Full Plate",
+            "slot"      : "torso",
+            "defence"   : 5
+        },
+
+        5   : {
+            "name"      : "Scale",
+            "slot"      : "torso",
+            "defence"   : 3
+        }
+    },
+
+    "legs"      : {
+
+        1   : {
+            "name"      : "Woolen Trousers",
+            "slot"      : "legs",
+            "defence"   : 1
+        },
+
+        2   : {
+            "name"      : "Chainmail Chausses",
+            "slot"      : "legs",
+            "defence"   : 3
+        },
+
+        3   : {
+            "name"      : "Scale Greaves",
+            "slot"      : "legs",
+            "defence"   : 4
+        },
+
+        4   : {
+            "name"      : "Plate Full Leg",
+            "slot"      : "legs",
+            "defence"   : 5
+        },
+
+        5   : {
+            "name"      : "Leather Cuisse",
+            "slot"      : "legs",
+            "defence"   : 2
+        }
+    },
+
+    "feet"     : {
+
+        1   : {
+            "name"      : "Leather Boots",
+            "slot"      : "feet",
+            "defence"   : 2
+        },
+
+        2   : {
+            "name"      : "Woolen Socks",
+            "slot"      : "feet",
+            "defence"   : 1
+        },
+
+        3   : {
+            "name"      : "Plate Sabatons",
+            "slot"      : "feet",
+            "defence"   : 5
+        },
+
+        4   : {
+            "name"      : "Steel Sabatons",
+            "slot"      : "feet",
+            "defence"   : 4
+        },
+
+        5   : {
+            "name"      : "Wooden Shoes",
+            "slot"      : "feet",
+            "defence"   : 3
+        }
+    },
+
+    "arms"      : {
+
+        1   : {
+            "name"      : "Leather Gloves",
+            "slot"      : "arms",
+            "defence"   : 3
+        },
+
+        2   : {
+            "name"      : "Plate Gauntlet",
+            "slot"      : "arms",
+            "defence"   : 3
+        },
+
+        3   : {
+            "name"      : "Steel Vambrace",
+            "slot"      : "arms",
+            "defence"   : 3
+        },
+
+        4   : {
+            "name"      : "Wooden Couter",
+            "slot"      : "arms",
+            "defence"   : 3
+        },
+
+        5   : {
+            "name"      : "Chainmail Rerebrace",
+            "slot"      : "arms",
+            "defence"   : 3
+        }
+    },
+
+    "right_hand"    : {
+
+        1   : {
+            "name"      : "Rusty Sword",
+            "slot"      : "right_hand",
+            "damage"    : 2
+        },
+
+        2   : {
+            "name"      : "Sharp Dagger",
+            "slot"      : "right_hand",
+            "damage"    : 3
+        },
+
+        3   : {
+            "name"      : "Mace",
+            "slot"      : "right_hand",
+            "damage"    : 5
+        },
+
+        4   : {
+            "name"      : "Kitchen Knife",
+            "slot"      : "right_hand",
+            "damage"    : 1
+        },
+
+        5   : {
+            "name"      : "Lance",
+            "slot"      : "right_hand",
+            "damage"    : 4
+        }
+    },
+
+    "left_hand"   : {
+
+        1   : {
+            "name"      : "Pan",
+            "slot"      : "left_hand",
+            "defence"   : 1
+        },
+
+        2   : {
+            "name"      : "Wooden Shield",
+            "slot"      : "left_hand",
+            "defence"   : 2
+        },
+
+        3   : {
+            "name"      : "Knight's Shield",
+            "slot"      : "left_hand",
+            "defence"   : 5
+        },
+
+        4   : {
+            "name"      : "Kite Shield",
+            "slot"      : "left_hand",
+            "defence"   : 3
+        },
+
+        5   : {
+            "name"      : "Buckler",
+            "slot"      : "left_hand",
+            "defence"   : 4
+        }
+    },
+
+    "potions"   : {
+        1   : {
+            "name"      : "Health",
+            "slot"      : "bag",
+            "health"    : 50
+        },
+
+        2   : {
+            "name"      : "Luck",
+            "slot"      : "bag",
+            "luck"      : 2
+        },
+
+        3   : {
+            "name"      : "Speed",
+            "slot"      : "bag",
+            "speed"     : 2
+        },
+
+        4   : {
+            "name"      : "Critical Strike",
+            "slot"      : "bag",
+            "c_strike"  : 2
+        },
+
+        5   : {
+            "name"      : "Mana",
+            "slot"      : "bag",
+            "mana"      : 50
+        }
+    }
+}
